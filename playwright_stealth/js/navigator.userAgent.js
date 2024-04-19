bb8af62f1715bc51dfa7ea0e8d40b0b40b6a738a
二毛 <76248124+erma0@users.noreply.github.com>
2024-04-19 14:29:25 +0800

    Update navigator.userAgent.js
    support msedge （HeadlessEdg/）

--- playwright_stealth/js/navigator.userAgent.js
+++ playwright_stealth/js/navigator.userAgent.js
@@ -1,5 +1,5 @@
 // replace Headless references in default useragent
 const current_ua = navigator.userAgent
 Object.defineProperty(Object.getPrototypeOf(navigator), 'userAgent', {
-    get: () => opts.navigator_user_agent || current_ua.replace('HeadlessChrome/', 'Chrome/')
+    get: () => opts.navigator_user_agent || current_ua.replace('Headless', '')
 })
